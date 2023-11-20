ff8fd6740986c90e043ea1ac79431109b994a00f
Kai Oestreich <51728041+Gisbert12843@users.noreply.github.com>
2023-11-21 00:25:50 +0100

    Create secret.h

---
+++ include/secret.h
@@ -0,0 +1,11 @@
+#pragma once
+#define openweather_api_key here_goes_your_api_key
+
+#define wifi_ssid "test_ssid"
+#define wifi_password "test_password"
+
+#define wifi_ssid2 "test_ssid2"
+#define wifi_password2 "test_password"
+
+#define wifi_ssid3 "test_ssid3"
+#define wifi_password3 "test_password"
